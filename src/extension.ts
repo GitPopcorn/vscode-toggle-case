6015e967f4eccbd34f57e5ce5cea18c60903739a
ZhouYi <370921291@qq.com>
2021-11-14 00:32:13 +0800

    Fix the problem of vscode #137118 [onDidChangeTextEditorSelection would not fire when select across word like Ctrl + Shift + Right].

--- src/extension.ts
+++ src/extension.ts
@@ -6,32 +6,81 @@ import { IEditBuilderContainer, IConfig } from './models';
 
 const CASE_CONTEXT = "CASE_CONTEXT";
 
+// CHANGED 2021111301 There is some problems for vscode to get text when watching changes
+//                    - When we choose selection with Ctrl + Shift + Left/Right, the event "onDidChangeTextEditorSelection" will not be fired.
+
+// CHANGED 2021111301 Extract the listener method for reusing, and adding a temp list
+const onSelectionChange = function(context: any, options: any, a: any[]) {
+	if(a[0].kind === undefined) {
+		return;
+	}
+	options = Object.assign({ updateAllTexts: true, updateAllTextsTemp: true, updateCurrentCase: true}, options);
+	const textEditor = vscode.window.activeTextEditor!;
+	const globalData = context.globalState.get(CASE_CONTEXT);
+	const allTexts: string[] = options.updateAllTexts 
+		? textEditor.selections.map((selection: vscode.Selection) => textEditor.document.getText(selection))
+		: globalData.allTexts
+	;
+	const allTextsTemp = options.updateAllTextsTemp ? allTexts.concat([]) : globalData.allTextsTemp;
+	const currentCase = options.updateCurrentCase ? CONFIG.ORIGINAL.caseName : globalData.currentCase;
+	const promise = globalData.promise;
+	context.globalState.update(CASE_CONTEXT, {allTexts, allTextsTemp, currentCase, promise});
+	
+};
+
+const arrayShallowEqual = function(arr1: any[], arr2: any[]) {
+	for (let index = 0; index < arr1.length; index++) {
+		const item1 = arr1[index];
+		const item2 = arr2[index];
+		if (item1 !== item2) {
+			return false;
+			
+		}
+		
+	}
+	return true;
+	
+};
+
 const logic = (context: vscode.ExtensionContext, changeCaseTo: string = "") => {
 	const config = vscode.workspace.getConfiguration('toggleCase.case');
-	const notification_config = vscode.workspace.getConfiguration('toggleCase.notification');
+	const notificationConfig = vscode.workspace.getConfiguration('toggleCase.notification');
 	const textEditor = vscode.window.activeTextEditor!;
-
+	
 	let editBuilderContainer: IEditBuilderContainer[] = [];
-
-	const globalData: any = context.globalState.get(CASE_CONTEXT)!;
-	let newCase = "";
-	let newRepresentation = "";
+	
+	// CHANGED 2021111301 Check if the current selected texts equal to temp texts, when not, read the selections again
+	let globalData: any = context.globalState.get(CASE_CONTEXT)!;
+	globalData.allTextsTemp = globalData.allTextsTemp || [];
+	let {
+		allTexts,
+		allTextsTemp,
+		currentCase
+	} = globalData;
+	var allTextsNow = textEditor.selections.map((selection: vscode.Selection) => textEditor.document.getText(selection));
+	var count = 1;
+	while (!(arrayShallowEqual(allTextsNow, allTextsTemp)) && (count ++ <= 10)) {
+		// If all the selection is empty, the bug #137118 may happen, so we try update the origin texts.
+		// After this bug fixed, this while loop and all the usage of "allTextsNow", "allTextsTemp" should be removed.
+		var updateAllTexts = (allTextsTemp.filter((text: string) => text).length === 0);
+		onSelectionChange(context, { updateAllTexts: updateAllTexts, updateCurrentCase: false }, [{ "kind": "nonsense" }]);
+		globalData = context.globalState.get(CASE_CONTEXT)!;
+		globalData.allTextsTemp = globalData.allTextsTemp || [];
+		allTexts = globalData.allTexts;
+		allTextsTemp = globalData.allTextsTemp;
+		currentCase = globalData.currentCase;
+		
+	}
+	const toggleTo = changeCaseTo || getNextCase(currentCase, config);
+	const toggleToObject = (CONFIG as {[x: string]: IConfig})[toggleTo];
+	const newRepresentation = toggleToObject.representation;
+	const newCase = toggleToObject.caseName;
 	textEditor.selections.forEach((selection: vscode.Selection, selectionIndex: number) => {
-
-		if(globalData === undefined || !Array.isArray(globalData.allTexts)  || globalData.allTexts.length < 1 || !globalData.currentCase){
+		if(globalData === undefined || !Array.isArray(allTexts)  || allTexts.length < 1 || !currentCase){
 			vscode.window.showInformationMessage("Error! Reload extension and VS Code. Apologies.");
 			return;
 		}
-
-		const {
-			allTexts,
-			currentCase
-		} = globalData;
-
-		const toggleTo = changeCaseTo || getNextCase(currentCase, config);
-		const toggleToObject = (CONFIG as {[x: string]: IConfig})[toggleTo];
-		newRepresentation = toggleToObject.representation;
-		newCase = toggleToObject.caseName;
+		
 		let modifiedCase = "";
 		if(selection.start.line === selection.end.line){
 			// single line
@@ -44,45 +93,37 @@ const logic = (context: vscode.ExtensionContext, changeCaseTo: string = "") => {
 		}
 		editBuilderContainer.push({selection, modifiedCase});
 	});
-
 	
 	textEditor.edit((editBuilder) => {
-		editBuilderContainer.forEach((element: IEditBuilderContainer) => {
+		editBuilderContainer.forEach((element: IEditBuilderContainer, index: number) => {
 			editBuilder.replace(element.selection, element.modifiedCase);
+			allTextsNow[index] = element.modifiedCase;
+			allTextsTemp[index] = element.modifiedCase;
 		});
 	});
-
+	
 	context.globalState.update(CASE_CONTEXT, {...globalData, currentCase: newCase});
-	if (notification_config.notifyOnChange)
+	if (notificationConfig.notifyOnChange) {
 		vscode.window.showInformationMessage(`Changed to ${newRepresentation}`);
-
+		
+	}
+	
 };
 
 export function activate(context: vscode.ExtensionContext) {
-
-	vscode.window.onDidChangeTextEditorSelection(function(...a) {
-		if(a[0].kind === undefined) {
-			return;
-		}
-		const textEditor = vscode.window.activeTextEditor!;
-		const allTexts: string[] = [];
-		const currentCase = CONFIG.ORIGINAL.caseName;
-		context.globalState.update(CASE_CONTEXT, {allTexts, currentCase});
-		textEditor.selections.forEach((selection: vscode.Selection) => {
-			const selectedText = textEditor.document.getText(selection);
-			allTexts.push(selectedText);
-		});
-		context.globalState.update(CASE_CONTEXT, {allTexts, currentCase});
-	});
-
+	
+	vscode.window.onDidChangeTextEditorSelection((...a) => onSelectionChange(context, {}, a));
+	
 	const SPECIFIC_KEYS = Object.values(CONFIG)
 		.filter((el: IConfig) => !["ORIGINAL"].includes(el.caseName))
 		.map((el: IConfig) => vscode.commands.registerCommand(`extension.changeCase.${el.configParam}`, () => logic(context, el.caseName)))
-
+	;
+	
 	context.subscriptions.push(
 		vscode.commands.registerCommand('extension.toggleCase', () => logic(context)),
 		...SPECIFIC_KEYS
 	);
+	
 }
 
 export function deactivate() {}
